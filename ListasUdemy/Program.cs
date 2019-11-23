845581b31b290bc1df6dd63568b4ab4c2e9917f2
Wagner Neves <wneves@outlook.com>
2019-11-22 22:49:57 -0300

    Projeto de estudo de listas csharp

---
+++ ListasUdemy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> listaLinhasArquivo = new List<string>();
+
+            listaLinhasArquivo.Add("Wagner");
+            listaLinhasArquivo.Add("joao");
+            listaLinhasArquivo.Add("Maria");
+            listaLinhasArquivo.Add("denis");
+            listaLinhasArquivo.Add("Adenis");
+
+            foreach (string item in listaLinhasArquivo)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(listaLinhasArquivo.Count);
+
+            string search = listaLinhasArquivo.Find(Procura);
+
+            Console.WriteLine(search);
+
+
+                  Console.Read();
+        }
+        static bool Procura(string s)
+        {
+            return s[0] == 'A';
+        }
+
+      
+    }
+}
